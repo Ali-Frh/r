def9a863379ef6539b6909b88c787d9e15065e80
siktirmirza <alicowman79@gmail.com>
2016-03-14 16:42:12 +0330

    Sudo Updated

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {25866548},
     admin_users = {},
     disabled_channels = {}
   }
